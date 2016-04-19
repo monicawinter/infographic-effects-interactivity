07152fa2371956600c3ee4137c3935bf8a380c85
monicawinter <m.m.winter@hotmail.com>
2016-04-19 15:51:03 -0400

    added waypoints

---
+++ js/waypoints.js
@@ -0,0 +1,54 @@
+var $kiss = $('.kiss');
+var $couple = $('.couple');
+var $dog = $('.dog');
+var $arrow = $('.arrow');
+var $arrow2 = $('.arrow2');
+var $arrow3 = $('.arrow3');
+
+$kiss.waypoint(function (direction) {
+  if (direction == 'down') {
+    $kiss.addClass('js-kiss-animate');
+  } else {
+    $kiss.removeClass('js-kiss-animate');
+  }
+}, { offset: '63%' });
+
+$couple.waypoint(function (direction) {
+  if (direction == 'down') {
+    $couple.addClass('js-couple-animate');
+  } else {
+    $couple.removeClass('js-couple-animate');
+  }
+}, { offset: '50%' });
+
+$dog.waypoint(function (direction) {
+  if (direction == 'down') {
+    $dog.addClass('js-dog-animate');
+  } else {
+    $dog.removeClass('js-dog-animate');
+  }
+}, { offset: '50%' });
+
+$arrow.waypoint(function (direction) {
+  if (direction == 'down') {
+    $arrow.addClass('arrow-animate');
+  } else {
+    $arrow.removeClass('arrow-animate');
+  }
+}, { offset: '100%' });
+
+$arrow2.waypoint(function (direction) {
+  if (direction == 'down') {
+    $arrow2.addClass('arrow2-animate');
+  } else {
+    $arrow2.removeClass('arrow2-animate');
+  }
+}, { offset: '100%' });
+
+$arrow3.waypoint(function (direction) {
+  if (direction == 'down') {
+    $arrow3.addClass('arrow3-animate');
+  } else {
+    $arrow3.removeClass('arrow3-animate');
+  }
+}, { offset: '100%' });
